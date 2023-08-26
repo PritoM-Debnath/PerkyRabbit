4e20f24f2f5b690f0aefb941e86c76436625987a
Pritom Debnath <debnathpritom@outlook.com>
2023-08-27 01:21:16 +0600

    Version 2

---
+++ ExpenseTracker/ExpenseTracker/Controllers/HomeController.cs
@@ -0,0 +1,96 @@
+﻿using ExpenseTracker.Data.Repositories;
+using ExpenseTracker.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Xml.Linq;
+
+namespace ExpenseTracker.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly IExpenseCategoryRepository categoryRepo;
+
+    public static IEnumerable<ExpenseCategory> AllExpenseCategories { get; set; }
+
+    public static bool IsCategoryUpdating=false;
+
+    public HomeController(IExpenseCategoryRepository expenseCategoryRepository)
+    {
+        categoryRepo = expenseCategoryRepository;
+    }
+
+    public IActionResult Index() => View();
+
+    public async Task<PartialViewResult> _AllCategories()
+    {
+        AllExpenseCategories = await Task.Run(() => categoryRepo.GetAllExpenseCategories());
+        return PartialView(AllExpenseCategories);
+    }
+    // post or not ?
+    public async Task<PartialViewResult> _SingleCategories(string _name)
+    {
+        var data = await Task.Run(() => categoryRepo.GetByName(_name));
+        return PartialView(data);
+    }
+
+    public async Task<bool> CheckDuplicateItem(string name)
+    {
+        bool isDuplicate = await Task.FromResult(AllExpenseCategories.Any(ec => ec.Name == name));
+        return isDuplicate;
+    }
+
+
+    [HttpGet]
+    public async Task<IActionResult> _EntryCategory(int _catID)
+    {
+        IsCategoryUpdating=true;
+
+        var data = categoryRepo.GetById(_catID);
+        if(data == null) { return NotFound(); }
+        else return View(data);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> _EntryCategory(ExpenseCategory _category)
+    {
+        if (!IsCategoryUpdating)
+        {
+            if (!string.IsNullOrEmpty(_category.Name))
+            {
+                if (await CheckDuplicateItem(_category.Name)) TempData["Message"] = @"<script> alert('Duplicate Name!!');</script>";
+                else
+                    if (ModelState.IsValid) await categoryRepo.AddExpenseCategory(_category);
+            }
+            else TempData["Message"] = @"<script> alert('No Category Name?');</script>";
+        }
+        else
+        {
+            if (ModelState.IsValid)
+            {
+                categoryRepo.UpdateExpenseCategory(_category);
+                IsCategoryUpdating = false;
+            }
+        }
+
+        return RedirectToAction("Index");
+    }
+
+    public async Task<IActionResult> RemoveCategory(ExpenseCategory category)
+    { 
+        if (ModelState.IsValid)
+        {
+            await categoryRepo.RemoveExpenseCategory(category);
+        }
+        return RedirectToAction("Index");
+    }
+
+
+    public IActionResult Privacy() => View();
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error() => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+}
+
+
+
+
---
+++ ExpenseTracker/ExpenseTracker/Data/Repositories/ExpenseEntryRepository.cs
@@ -0,0 +1,52 @@
+﻿using ExpenseTracker.Models;
+
+namespace ExpenseTracker.Data.Repositories
+{
+    public class ExpenseEntryRepository : IExpenseEntryRepository
+    {
+        private readonly AppDataContext appDataContext;
+
+        public ExpenseEntryRepository(AppDataContext _appDataContext) => this.appDataContext = _appDataContext;
+
+        public IEnumerable<ExpenseEntry> GetAllExpenseEntries()
+        {
+            return appDataContext.ExpenseEntries.ToList();
+        }
+
+        public ExpenseEntry? GetById(int _id)
+        {
+            return appDataContext.ExpenseEntries.FirstOrDefault(c => c.Id == _id) ?? null;
+        }
+
+        public IEnumerable<ExpenseEntry>? GetExpenseEntriesByCategory(ExpenseCategory expenseCategory)
+        {
+            return appDataContext.ExpenseCategories.Find(expenseCategory).Expenses ?? null;
+        }
+
+        public bool AddExpenseEntryForCategory(ExpenseCategory expenseCategory,ExpenseEntry entry)
+        {
+            appDataContext.ExpenseCategories.Find(expenseCategory).Expenses.Add(entry);
+            var cc = appDataContext.SaveChanges();
+
+            return bool.Parse(cc.ToString());
+
+        }
+
+        public void RemoveExpenseCategory(ExpenseCategory expenseCategory)
+        {
+            appDataContext.ExpenseCategories.Remove(expenseCategory);
+            appDataContext.SaveChangesAsync();
+        }
+
+        public void UpdateExpenseCategory(ExpenseCategory expenseCategory)
+        {
+            appDataContext.ExpenseCategories.Update(expenseCategory);
+            appDataContext.SaveChangesAsync();
+        }
+
+        public Task SaveChangesAsync()
+        {
+            return appDataContext.SaveChangesAsync();
+        }
+    }
+}
